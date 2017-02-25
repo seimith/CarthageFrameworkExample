dd7a4b1bbc748c591f5d2635840ecbeafab73669
= <seimith@gmail.com>
2017-02-25 10:55:52 -0800

    Create CocoaPod Framework

---
+++ CarthageFrameworkExample/CarthageFrameworkExample.h
@@ -0,0 +1,19 @@
+//
+//  CarthageFrameworkExample.h
+//  CarthageFrameworkExample
+//
+//  Created by Seimith on 2/25/17.
+//  Copyright © 2017 Seimith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CarthageFrameworkExample.
+FOUNDATION_EXPORT double CarthageFrameworkExampleVersionNumber;
+
+//! Project version string for CarthageFrameworkExample.
+FOUNDATION_EXPORT const unsigned char CarthageFrameworkExampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CarthageFrameworkExample/PublicHeader.h>
+
+
